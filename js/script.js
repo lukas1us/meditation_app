242762e76393c7ad57a0bde0a058682d18fe63e3
lukas1us <lukas1us@users.noreply.github.com>
2020-06-30 18:46:25 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breatheAnimation()
+
+//"classic" breathing exercise by Brad Traversy
+function breatheAnimation() {
+    text.innerHTML = 'Breathe in!'
+    container.className = 'container grow'
+
+    setTimeout(() => {
+        text.innerHTML = 'Hold'
+
+        setTimeout(() => {
+            text.innerHTML = 'Breathe Out !'
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breatheTime)
+}
+
+setInterval(breatheAnimation, totalTime);
