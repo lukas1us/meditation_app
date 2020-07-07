7f796d3b6c395450e7a785fcda93b280cd511065
lukas1us <lukas1us@users.noreply.github.com>
2020-07-07 14:51:32 +0200

    background fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,26 @@
-const container = document.querySelector('.container')
-const text = document.querySelector('#text')
+const container = document.querySelector(".container");
+const text = document.querySelector("#text");
 
-const totalTime = 7500
-const breatheTime = (totalTime / 5) * 2
-const holdTime = totalTime / 5
-
-breatheAnimation()
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+document.body.style.backgroundImage =
+    "url('/dist/img/background.jpg')";
+breatheAnimation();
 
 //"classic" breathing exercise by Brad Traversy
 function breatheAnimation() {
-    text.innerHTML = 'Breathe in!'
-    container.className = 'container grow'
+    text.innerHTML = "Breathe in!";
+    container.className = "container grow";
 
     setTimeout(() => {
-        text.innerHTML = 'Hold'
+        text.innerHTML = "Hold";
 
         setTimeout(() => {
-            text.innerHTML = 'Breathe Out !'
-            container.className = 'container shrink'
-        }, holdTime)
-    }, breatheTime)
+            text.innerHTML = "Breathe Out !";
+            container.className = "container shrink";
+        }, holdTime);
+    }, breatheTime);
 }
 
 setInterval(breatheAnimation, totalTime);
